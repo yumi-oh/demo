460d4b3bed6843ef949efd6098572c397197b018
yumi-oh <58407203+yumi-oh@users.noreply.github.com>
2021-12-24 15:43:32 +0900

    ch

--- demo/src/main/java/com/gr/herb/test/TestController.java
+++ demo/src/main/java/com/gr/herb/test/TestController.java
@@ -3,7 +3,7 @@ package com.gr.herb.test;
 public class TestController {
 
 	public void func() {
-		System.out.println("hello everyone~~ by chief");
+		System.out.println("hello everyone~~!!! by chief");
 		System.out.println("func~~");
 	}
 }
