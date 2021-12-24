18f09fdd0e45ef3165d5b160a6b7866626ece74e
yumi-oh <58407203+yumi-oh@users.noreply.github.com>
2021-12-24 15:40:34 +0900

    oh!

--- demo/src/main/java/com/gr/demo/DemoApplication.java
+++ demo/src/main/java/com/gr/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hi by hong!");
+		System.out.println("hi by oh~~~~!!!!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
